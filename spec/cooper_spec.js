86beb6b01db530f0e0f7872cdade80698cca7660
kalle <kmthorsnes@gmail.com>
2018-02-02 14:50:46 +0100

    passed test excellent

--- spec/cooper_spec.js
+++ spec/cooper_spec.js
@@ -1,8 +1,8 @@
-describe("Athlete", function() {
-      var athlete;
+describe("Person", function() {
+      var person;
 
         beforeEach(function() {
-          athlete = new Athlete({
+          person = new Person({
             age: 32,
             gender: 'Male',
             distance: 4000
@@ -10,14 +10,23 @@ describe("Athlete", function() {
       });
 
       it('Should return an age of 32', function() {
-        expect(athlete.age).toEqual(32);
+        expect(person.age).toEqual(32);
       });
 
       it('Should return male as gender', function() {
-        expect(athlete.gender).toEqual('Male');
+        expect(person.gender).toEqual('Male');
       });
 
       it('Should return 4000 as distance', function() {
-          expect(athlete.distance).toEqual(4000);
+          expect(person.distance).toEqual(4000);
       });
+
+      it('should return string "male"', function() {
+          expect(result(person)).toEqual('Male');
+      });
+
+      it('Should return "excellent" as male 32, 4000 meters run', function() {
+          expect(excellent(person)).toEqual('Excellent');
+      });
+
   });
--- src/cooper.js
+++ src/cooper.js
@@ -1,5 +1,30 @@
-function Athlete(attr) {
+function Person(attr) {
   this.age = attr.age;
   this.gender = attr.gender;
   this.distance = attr.distance;
-};
+}
+
+function result(person) {
+  if (person.gender === 'Male') {
+    return 'Male';
+  }
+}
+
+function excellent(person) {
+  if (person.gender === 'Male' &&
+    person.age >= 30 && person.age <= 39 &&
+    person.distance > 3000
+  ) {
+    return 'Excellent';
+  }
+}
+
+function aboveaverage(person) {
+  if (person.gender === 'Male' &&
+    person.age >= 30 && person.age <= 39 &&
+    person.distance >= 2300 && person.distance <= 2699
+  )
+  {
+    return 'Above average';
+  }
+}

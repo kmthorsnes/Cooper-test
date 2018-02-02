f61c3ca7e7d112cbb502a783538e3aa46d9bc3d6
kalle <kmthorsnes@gmail.com>
2018-02-02 13:46:04 +0100

    added test for distance and gender

--- spec/cooper_spec.js
+++ spec/cooper_spec.js
@@ -1,13 +1,23 @@
 describe("Athlete", function() {
-  var athlete;
+      var athlete;
 
-  beforeEach(function() {
-    athlete = new Athlete({age: 32, gender: 'Male', distance: 4000});
+        beforeEach(function() {
+          athlete = new Athlete({
+            age: 32,
+            gender: 'Male',
+            distance: 4000
+        });
+      });
+
+      it('Should return an age of 32', function() {
+        expect(athlete.age).toEqual(32);
+      });
+
+      it('Should return male as gender', function() {
+        expect(athlete.gender).toEqual('Male');
+      });
+
+      it('Should return 4000 as distance', function() {
+          expect(athlete.distance).toEqual(4000);
+      });
   });
-
-  it('Should return an age of 32', function(){
-    expect(athlete.age).toEqual(32);
-  });
-
-
-});
